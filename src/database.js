fa3d9ddc03c5b8d3b421ca2098656055fd0bded7
Felipe Michalski <felimichalski@icloud.com>
2022-07-05 18:55:48 -0300

    Database uri changed

--- src/database.js
+++ src/database.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://localhost/tortilleria')
+const connection = mongoose.connect(process.env.DB_URI)
     .then(() => console.log('Database is connected'))
     .catch(err => console.error(err));
